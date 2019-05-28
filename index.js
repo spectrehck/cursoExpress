558731275bf494cd025be4d8d87277e89fbe46b5
cesar <omar18pe@gmail.com>
2019-05-28 13:48:27 -0500

    creando la conexion con express

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const app = express();
 app.get('/', function(req, res, next) {
     res.send("hello world")
 })
+
+// iniciar el server
+
+const server = app.listen(8000, function() {
+    console.log(`listening  http://localhost:$(server.address().port )`);
+});
